34a2e0910aa8251fa9ad3cd7a8a9668f79885c80
Focus4Study <f4studies@gmail.com>
2025-04-09 02:12:47 +0600

    initial commit

---
+++ helloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Using c means character and out means output" << std::endl;
+    std::cout << "endl means end line. Its benefit is that it flushes the output buffer" << std::endl; 
+    std::cout << "Using new line character" << '\n';
+    std::cout << "The new line character just adds a new line. Its better performance wise." << '\n';
+    return 0;
+}
